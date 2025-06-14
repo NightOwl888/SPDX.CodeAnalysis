a21925069eb7ee36aa02c4c481acde05abaf5f82
Shad Storhaug <shad@shadstorhaug.com>
2025-06-15 01:25:34 +0700

    Revert "WIP: Fix for broken fallback by changing cache"
    This reverts commit 0dcf58389cf66aca2166034d3d4a8f572b545cc8.

--- src/SPDX.CodeAnalysis/Utility/StringKey.cs
+++ src/SPDX.CodeAnalysis/Utility/StringKey.cs
@@ -6,55 +6,33 @@ namespace SPDX.CodeAnalysis
 {
     public sealed class StringKey
     {
-        private readonly string _spdxIdentifier;
-        private readonly string _licenseLocation;
+        private readonly string key;
 
-        public StringKey(string spdxIdentifier, string licenseLocation)
+        public StringKey(string key)
         {
-            _spdxIdentifier = spdxIdentifier ?? throw new ArgumentNullException(nameof(spdxIdentifier));
-            _licenseLocation = licenseLocation ?? throw new ArgumentNullException(nameof(licenseLocation));
+            this.key = key;
         }
 
-        public bool Equals(ReadOnlySpan<char> spdxIdentifier, ReadOnlySpan<char> licenseLocation)
-            => _spdxIdentifier.AsSpan().SequenceEqual(spdxIdentifier) && _licenseLocation.AsSpan().SequenceEqual(licenseLocation);
+        public bool Equals(ReadOnlySpan<char> str)
+            => key.AsSpan().Equals(str, StringComparison.Ordinal);
 
         public override bool Equals(object obj)
         {
-            if (obj is StringKey str)
-                return Comparer.Equals(this, str);
+            if (obj is string str)
+                return key.Equals(str, StringComparison.Ordinal);
 
             return false;
         }
 
         public override int GetHashCode()
-            => GetHashCode(_spdxIdentifier.AsSpan(), _licenseLocation.AsSpan());
-
-        public static int GetHashCode(ReadOnlySpan<char> spdxIdentifier, ReadOnlySpan<char> licenseLocation)
-        {
-            int hash = 17;
-            hash = hash * 31 + StringHelper.GetHashCode(spdxIdentifier);
-            hash = hash * 31 + StringHelper.GetHashCode(licenseLocation);
-            return hash;
-        }
+            => StringHelper.GetHashCode(key.AsSpan());
 
         public static KeyComparer Comparer { get; } = new KeyComparer();
 
         public sealed class KeyComparer : IEqualityComparer<StringKey>
         {
             public bool Equals(StringKey? x, StringKey? y)
-            {
-                if (x is null)
-                    return y is null;
-                if (y is null)
-                    return false;
-
-                if (!x._spdxIdentifier.AsSpan().SequenceEqual(y._spdxIdentifier.AsSpan()))
-                    return false;
-                if (!x._licenseLocation.AsSpan().SequenceEqual(y._licenseLocation.AsSpan()))
-                    return false;
-
-                return true;
-            }
+                => x?.key.Equals(y?.key, StringComparison.Ordinal) ?? false;
 
             public int GetHashCode(StringKey obj)
                 => obj.GetHashCode();
