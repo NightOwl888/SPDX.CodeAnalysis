18478199f6088486f53f76c67707f01fd9fd6992
Shad Storhaug <shad@shadstorhaug.com>
2025-08-11 23:08:13 +0700

    Moved FilePosition enum from SPDX.CodeAnalysis.Tests to SPDX.Common, since we want to use this for code fixes.

--- src/SPDX.Common/FilePosition.cs
+++ src/SPDX.Common/FilePosition.cs
@@ -1,8 +1,11 @@
 ﻿// Use of this source code is governed by an MIT-style license that can be
 // found in the LICENSE.txt file or at https://opensource.org/licenses/MIT.
 
-namespace SPDX.CodeAnalysis.Tests
+namespace SPDX.CodeAnalysis
 {
+    /// <summary>
+    /// The position of a given text element.
+    /// </summary>
     public enum FilePosition
     {
         BeforeUsings,
