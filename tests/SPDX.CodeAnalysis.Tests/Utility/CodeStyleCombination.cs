ba116a53d61f72f2175d887c2696bb2871cf54ee
Shad Storhaug <shad@shadstorhaug.com>
2025-08-11 22:56:57 +0700

    SPDX.CodeAnalysis.Tests: Cleaned up dead code and using directives

--- tests/SPDX.CodeAnalysis.Tests/Utility/CodeStyleCombination.cs
+++ tests/SPDX.CodeAnalysis.Tests/Utility/CodeStyleCombination.cs
@@ -1,12 +1,8 @@
 ﻿// Use of this source code is governed by an MIT-style license that can be
 // found in the LICENSE.txt file or at https://opensource.org/licenses/MIT.
 
-using SPDX.CodeAnalysis.Tests.CSharp;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SPDX.CodeAnalysis.Tests
 {
@@ -50,97 +46,4 @@ namespace SPDX.CodeAnalysis.Tests
                             yield return new CodeStyleCombination(nsStyle, id, copy, lic);
         }
     }
-
-    //public sealed class CodeStyleCombination
-    //{
-    //    public required CodeLanguage Language { get; init; }
-    //    public required NamespaceStyle NamespaceStyle { get; init; }
-    //    public required CommentStyle CommentStyle { get; init; }
-    //    public required FilePosition Position { get; init; }
-
-    //    public string DefaultSourceFilePath => Language switch
-    //    {
-    //        CodeLanguage.CSharp => "project/src/specialized/stuff/foo.cs",
-    //        CodeLanguage.VisualBasic => "project/src/specialized/stuff/foo.vb",
-    //        _ => throw new NotSupportedException()
-    //    };
-
-    //    public string GenerateTestCode(
-    //        LicenseComponent components,
-    //        string? licenseIdentifierText,
-    //        string? copyrightText,
-    //        string? licenseText)
-    //    {
-    //        IFileBuilder builder = Language switch
-    //        {
-    //            CodeLanguage.CSharp => new CSharpFileBuilderAdapter(NamespaceStyle),
-    //            // TODO: Add Visual Basic implementation when ready
-    //            _ => throw new NotSupportedException()
-    //        };
-
-    //        if (components.HasFlag(LicenseComponent.LicenseIdentifier))
-    //        {
-    //            builder.WriteComment(Position, licenseIdentifierText ?? "SPDX-License-Identifier: " + Constants.License.Apache2.SPDX_LicenseIdentifier, CommentStyle);
-    //        }
-
-    //        if (components.HasFlag(LicenseComponent.CopyrightText))
-    //        {
-    //            builder.WriteComment(Position, copyrightText ?? "SPDX-FileCopyrightText: " + Constants.License.Apache2.SPDX_FileCopyrightText, CommentStyle);
-    //        }
-
-    //        if (components.HasFlag(LicenseComponent.LicenseText))
-    //        {
-    //            builder.WriteComment(Position, licenseText ?? Constants.License.Apache2.Header, CommentStyle);
-    //        }
-
-    //        return builder.ToString();
-    //    }
-
-    //    public static IEnumerable<CodeStyleCombination> AllFor(CodeLanguage language)
-    //    {
-    //        var positions = new[]
-    //        {
-    //            FilePosition.BeforeUsings,
-    //            FilePosition.BeforeNamespace,
-    //            FilePosition.BeforeType
-    //        };
-
-    //        var commentStyles = new[]
-    //        {
-    //            CommentStyle.SingleLine,
-    //            CommentStyle.MultiLine
-    //        };
-
-    //        var namespaceStyles = language switch
-    //        {
-    //            CodeLanguage.CSharp => new[] {
-    //                NamespaceStyle.Global,
-    //                NamespaceStyle.FileScoped,
-    //                NamespaceStyle.BlockScoped
-    //            },
-    //            CodeLanguage.VisualBasic => new[] {
-    //                NamespaceStyle.BlockScoped // VB only supports block style
-    //            },
-    //            _ => throw new NotSupportedException()
-    //        };
-
-    //        foreach (var nsStyle in namespaceStyles)
-    //            foreach (var cmStyle in commentStyles)
-    //                foreach (var position in positions)
-    //                {
-    //                    yield return new CodeStyleCombination
-    //                    {
-    //                        Language = language,
-    //                        NamespaceStyle = nsStyle,
-    //                        CommentStyle = cmStyle,
-    //                        Position = position
-    //                    };
-    //                }
-    //    }
-
-    //    public override string ToString()
-    //    {
-    //        return $"{Language}-{NamespaceStyle}-{CommentStyle}-{Position}";
-    //    }
-    //}
 }
--- tests/SPDX.CodeAnalysis.Tests/Utility/CommentWriter.cs
+++ tests/SPDX.CodeAnalysis.Tests/Utility/CommentWriter.cs
@@ -4,7 +4,6 @@
 using System;
 using System.IO;
 using System.Text;
-using static SPDX.CodeAnalysis.MemoryExtensions;
 
 namespace SPDX.CodeAnalysis.Tests
 {
