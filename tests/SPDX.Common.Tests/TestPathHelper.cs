8186f44eeb34ffa6bb586fa926a4a46c723eb6c7
Shad Storhaug <shad@shadstorhaug.com>
2025-08-23 23:48:26 +0700

    Added tests for PathHelper.NormalizeAndCombine() for both Windows and Unix style paths.

---
+++ tests/SPDX.Common.Tests/TestPathHelper.cs
@@ -0,0 +1,113 @@
+using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using SPDX.CodeAnalysis;
+
+namespace SPDX.Common.Tests
+{
+    public class TestPathHelper
+    {
+        private static readonly bool IsWindows = OperatingSystem.IsWindows();
+        public static IEnumerable<object[]> Combine_CommonCases_TestData()
+        {
+            if (IsWindows)
+            {
+                // Joining relative paths
+                yield return new object[] { new string[] { "foo", "bar" }, false, "foo\bar" };
+                yield return new object[] { new string[] { "foo/bar/baz", "stuff" }, false, @"foo\bar\baz\stuff" };
+                yield return new object[] { new string[] { @"foo\bar\baz", "stuff" }, false, @"foo\bar\baz\stuff" };
+                yield return new object[] { new string[] { @"foo\bar\baz", "" }, true, @"foo\bar\baz\" };
+                yield return new object[] { new string[] { @"foo\bar\baz", "" }, false, @"foo\bar\baz" };
+
+                yield return new object[] { new string[] { "foo/bar/baz", @"stuff\specialized" }, false, @"foo\bar\baz\stuff\specialized" };
+                yield return new object[] { new string[] { @"foo\bar\baz", "stuff/specialized" }, false, @"foo\bar\baz\stuff\specialized" };
+
+                yield return new object[] { new string[] { "foo/bar/baz", @"stuff\specialized" + '\\' }, false, @"foo\bar\baz\stuff\specialized" + '\\' };
+                yield return new object[] { new string[] { @"foo\bar\baz", "stuff/specialized/" }, false, @"foo\bar\baz\stuff\specialized" + '\\' };
+
+                yield return new object[] { new string[] { "foo/bar/baz", @"stuff\specialized" }, true, @"foo\bar\baz\stuff\specialized" + '\\' };
+                yield return new object[] { new string[] { @"foo\bar\baz", "stuff/specialized" }, true, @"foo\bar\baz\stuff\specialized" + '\\' };
+
+                // Joining rooted paths
+                yield return new object[] { new string[] { @"C:\foo", "bar" }, false, @"C:\foo\bar" };
+                yield return new object[] { new string[] { @"C:\foo\bar\baz", "stuff" }, false, @"C:\foo\bar\baz\stuff" };
+                yield return new object[] { new string[] { @"C:\foo\bar\baz", "stuff" }, true, @"C:\foo\bar\baz\stuff\" };
+                yield return new object[] { new string[] { @"C:\foo\bar\baz", "" }, true, @"C:\foo\bar\baz\" };
+                yield return new object[] { new string[] { @"C:\foo\bar\baz", "" }, false, @"C:\foo\bar\baz" };
+
+                yield return new object[] { new string[] { "C:/foo/bar/baz", @"stuff\specialized" }, false, @"C:\foo\bar\baz\stuff\specialized" };
+                yield return new object[] { new string[] { @"C:\foo\bar\baz", "stuff/specialized" }, false, @"C:\foo\bar\baz\stuff\specialized" };
+
+                yield return new object[] { new string[] { "C:/foo/bar/baz", @"stuff\specialized\" }, false, @"C:\foo\bar\baz\stuff\specialized\" };
+                yield return new object[] { new string[] { @"C:\foo\bar\baz", "stuff/specialized/" }, false, @"C:\foo\bar\baz\stuff\specialized\" };
+
+                yield return new object[] { new string[] { "C:/foo/bar/baz", @"stuff\specialized" }, true, @"C:\foo\bar\baz\stuff\specialized\" };
+                yield return new object[] { new string[] { @"C:\foo\bar\baz", "stuff/specialized" }, true, @"C:\foo\bar\baz\stuff\specialized\" };
+                yield return new object[] { new string[] { @"C:\foo\bar\baz", @"stuff/specialized\" }, true, @"C:\foo\bar\baz\stuff\specialized\" };
+                
+                // Check removal of multiple consecutive delimiters
+                yield return new object[] { new string[] { @"C:\foo\\bar\baz", @"stuff\\specialized\" }, false, @"C:\foo\bar\baz\stuff\specialized\" };
+                yield return new object[] { new string[] { @"C:\foo\\/\\bar\baz", @"stuff\\/\\specialized\" }, false, @"C:\foo\bar\baz\stuff\specialized\" };
+            }
+            else // Unix-style paths
+            {
+                // Joining relative paths
+                yield return new object[] { new string[] { "foo", "bar" }, false, "foo/bar" };
+                yield return new object[] { new string[] { "foo/bar/baz", "stuff" }, false, "foo/bar/baz/stuff" };
+                
+                // Note that in Unix-style paths, \ is a valid character within a folder or file name.
+                // So, we expect it not to be normalized.
+                
+                yield return new object[] { new string[] { @"foo\bar\baz", "stuff" }, false, @"foo\bar\baz/stuff" };
+                yield return new object[] { new string[] { "foo/bar/baz", "" }, true, "foo/bar/baz/" };
+                yield return new object[] { new string[] { "foo/bar/baz", "" }, false, "foo/bar/baz" };
+                
+                yield return new object[] { new string[] { "foo/bar/baz", @"stuff\specialized" }, false, @"foo/bar/baz/stuff\specialized" };
+                yield return new object[] { new string[] { @"foo\bar\baz", "stuff/specialized" }, false, @"foo\bar\baz/stuff/specialized" };
+
+                yield return new object[] { new string[] { "foo/bar/baz", @"stuff\specialized\" }, false, @"foo/bar/baz/stuff\specialized\" };
+                yield return new object[] { new string[] { @"foo\bar\baz", "stuff/specialized/" }, false, @"foo\bar\baz/stuff/specialized/" };
+                
+                yield return new object[] { new string[] { "foo/bar/baz", @"stuff\specialized" }, true, @"foo/bar/baz/stuff\specialized/" };
+                yield return new object[] { new string[] { @"foo\bar\baz", "stuff/specialized" }, true, @"foo\bar\baz/stuff/specialized/" };
+                
+                // Joining rooted paths
+                yield return new object[] { new string[] { "/foo", "bar" }, false, "/foo/bar" };
+                yield return new object[] { new string[] { "/foo/bar/baz", "stuff" }, false, "/foo/bar/baz/stuff" };
+                
+                // Note that in Unix-style paths, \ is a valid character within a folder or file name.
+                // So, we expect it not to be normalized.
+                
+                yield return new object[] { new string[] { @"/foo\bar\baz", "stuff" }, false, @"/foo\bar\baz/stuff" };
+                yield return new object[] { new string[] { "/foo/bar/baz", "" }, true, "/foo/bar/baz/" };
+                yield return new object[] { new string[] { "/foo/bar/baz", "" }, false, "/foo/bar/baz" };
+                
+                yield return new object[] { new string[] { @"/foo/bar/baz", @"stuff\specialized" }, false, @"/foo/bar/baz/stuff\specialized" };
+                yield return new object[] { new string[] { @"\foo\bar\baz", "stuff/specialized" }, false, @"\foo\bar\baz/stuff/specialized" };
+
+                yield return new object[] { new string[] { "/foo/bar/baz", @"stuff\specialized\" }, false, @"/foo/bar/baz/stuff\specialized\" };
+                yield return new object[] { new string[] { @"\foo\bar\baz", "stuff/specialized/" }, false, @"\foo\bar\baz/stuff/specialized/" };
+                
+                yield return new object[] { new string[] { "/foo/bar/baz", @"stuff\specialized" }, true, @"/foo/bar/baz/stuff\specialized/" };
+                yield return new object[] { new string[] { @"\foo\bar\baz", "stuff/specialized" }, true, @"\foo\bar\baz/stuff/specialized/" };
+                yield return new object[] { new string[] { "/foo/bar/baz/", "stuff/specialized/" }, true, @"/foo/bar/baz/stuff/specialized/" };
+                
+                // Check removal of multiple consecutive delimiters
+                yield return new object[] { new string[] { "/foo//bar/baz", "stuff//specialized/" }, false, @"/foo/bar/baz/stuff/specialized/" };
+                yield return new object[] { new string[] { "/foo/////bar/baz", "stuff/////specialized/" }, false, @"/foo/bar/baz/stuff/specialized/" }; 
+            }
+
+        }
+        
+        [TestCaseSource(nameof(Combine_CommonCases_TestData))]
+        public void TestNormalizeAndCombine(string[] paths, bool ensureTrailingSlash, string expected)
+        {
+            Assert.That(paths, Is.Not.Null);
+            Assert.That(paths.Length, Is.EqualTo(2));
+
+            string actual = PathHelper.NormalizeAndCombine(paths[0].AsSpan(), paths[1].AsSpan(), ensureTrailingSlash);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
